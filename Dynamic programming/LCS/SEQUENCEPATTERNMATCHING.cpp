22ca8d17fe29d16790921c66756bf3e5cc24052d
ANAMAYA VYAS <68732493+anamaya-vyas-zlatan@users.noreply.github.com>
2021-06-20 03:15:02 +0530

    SEQUENCE PATTERN MATCHING

---
+++ Dynamic programming/LCS/SEQUENCEPATTERNMATCHING.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     string x,y;
+    cout<<"Enter string a: ";
+    cin>>x;
+    cout<<"Enter string b: ";
+    cin>>y;
+    int n, m;
+    n = x.length();
+    m = y.length();
+    int t[n+1][m+1];
+
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=m;j++)
+        {
+            if(i==0 || j==0)
+            {
+                t[i][j]=0;
+            }
+            else if(x[i-1] == y[j-1])
+            {
+                t[i][j] = 1+t[i-1][j-1];
+            }
+            else{
+                t[i][j] = max(t[i-1][j], t[i][j-1]);
+            }
+        }
+    }
+    int ans = t[n][m];
+    if(ans == min(n,m))
+    {
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";    }
+}
